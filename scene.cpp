142deb04f58bcad4da2a8214e1ad7aa7079ddc67
acanois <david.richter42@gmail.com>
2025-03-23 13:26:45 -0500

    cylinder

--- scene.cpp
+++ scene.cpp
@@ -7,7 +7,7 @@
 
 Scene::Scene()
     : mCamera(std::make_unique<SceneCamera>(
-        Vector3 { 10.f, 10.f, 10.f },
+        Vector3 { 0.f, 0.f, -10.f },
         Vector3 { 0.f, 0.f, 0.f },
         Vector3 { 0.f, 1.f, 0.f },
         45.f,
@@ -29,8 +29,18 @@ void Scene::draw() {
     ClearBackground(Color { 37, 36, 34, 255 });
     mCamera->beginMode3d();
 
-    DrawCube(Vector3 { 0.f, 0.f, 0.f }, 2.0f, 2.0f, 2.0f, Color { 55, 114, 255, 255 });
-    DrawCubeWires(Vector3 { 0.f, 0.f, 0.f }, 2.0f, 2.0f, 2.0f, Color { 243, 41, 53, 255 });
+    auto height = 6.f;
+    auto yPosition = height - (height / 2.f);
+    DrawCylinder(
+        Vector3 { 0.f, -yPosition, 0.f },
+        1.0f, 1.0f, height, 16.f,
+        Color { 55, 114, 255, 255 }
+    );
+//    DrawCubeWires(
+//        Vector3 { 0.f, 0.f, 0.f },
+//        2.0f, 2.0f, 2.0f,
+//        Color { 243, 41, 53, 255 }
+//    );
 
     DrawGrid(10, 1.0f);
 
