788faf906ae69ee04e738116ddb1fb3d2803fcf3
acanois <david.richter42@gmail.com>
2025-03-28 11:04:57 -0500

    take out skybox for now

--- scene.cpp
+++ scene.cpp
@@ -56,64 +56,11 @@ void Scene::run() {
 
     rlSetBlendMode(RL_BLEND_DST_ALPHA);
 
-    // Render Texture
-    RenderTexture2D target = LoadRenderTexture(1280, 720);
-
-    // Skybox
-    Mesh cube = GenMeshCube(100.f, 100.f, 100.f);
-    Model skybox = LoadModelFromMesh(cube);
-
-    auto useHDR = false;
-    skybox.materials[0].shader = loadShader("skybox.vs", "skybox.fs");
-    SetShaderValue(
-        skybox.materials[0].shader,
-        GetShaderLocation(skybox.materials[0].shader, "environmentMap"),
-        (int[1]){ MATERIAL_MAP_CUBEMAP },
-        SHADER_UNIFORM_INT
-    );
-    SetShaderValue(
-        skybox.materials[0].shader,
-        GetShaderLocation(skybox.materials[0].shader, "doGamma"),
-        (int[1]) { useHDR ? 1 : 0 },
-        SHADER_UNIFORM_INT);
-    SetShaderValue(
-        skybox.materials[0].shader,
-        GetShaderLocation(skybox.materials[0].shader, "vflipped"),
-        (int[1]){ useHDR ? 1 : 0 },
-        SHADER_UNIFORM_INT
-    );
-
-    // Cubemap for skybox
-    Shader shdrCubemap = loadShader("cubemap.vs", "cubemap.fs");
-    SetShaderValue(
-        shdrCubemap,
-        GetShaderLocation(shdrCubemap, "equirectangularMap"),
-        (float[1]) { 0 },
-        SHADER_UNIFORM_FLOAT
-    );
-
-    Image skyboxTexture = loadImage("skybox-blue-night.png");
-    skybox.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture = LoadTextureCubemap(
-        skyboxTexture,
-        CUBEMAP_LAYOUT_AUTO_DETECT
-    );
-    UnloadImage(skyboxTexture);
-
-    // Postprocessing Bloom Shader
-    auto shdrBloom = loadShader("bloom.fs");
-
     while (!WindowShouldClose()) {
-        BeginTextureMode(target);
+        BeginDrawing();
         ClearBackground(Color { 7, 6, 4, 255 });
 
         mCamera->beginMode3d();
-
-        // Draw the skybox
-        rlDisableBackfaceCulling();  // To see skybox texture
-        rlDisableDepthMask();
-            DrawModel(skybox, Vector3 { 0.f, 0.f, 0.f }, 1.f, WHITE);
-        rlEnableBackfaceCulling();
-        rlEnableDepthMask();
 
         mCrystal->draw();
 
@@ -121,30 +68,9 @@ void Scene::run() {
         if (mShowGrid) DrawGrid(10, 1.0f);
 
         mCamera->endMode3d();
-        EndTextureMode();
-
-        BeginDrawing();
-        ClearBackground(Color { 7, 6, 4, 255 });
-
-        DrawTextureRec(
-            target.texture,
-            Rectangle {
-                0, 0,
-                static_cast<float>(target.texture.width),
-                static_cast<float>(-target.texture.height) // Inverted because OpenGL coordinates
-            },
-            Vector2 { 0, 0 },
-            WHITE
-        );
         EndDrawing();
         update();
     }
-
-    UnloadShader(shdrCubemap);
-    UnloadShader(skybox.materials[0].shader);
-    UnloadShader(shdrBloom);
-    UnloadRenderTexture(target);
-    UnloadModel(skybox);
 
     CloseWindow();
 }
